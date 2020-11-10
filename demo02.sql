b1d89df7dbf593c9d1af93cbe66deb2cc3850cc9
Vincent <57202594+VincnetLinPC@users.noreply.github.com>
2020-11-10 21:10:56 +0000

    Add files via upload

---
+++ demo02.sql
@@ -0,0 +1,10 @@
+ CREATE TABLE Student (
+ regno INTEGER NOT NULL PRIMARY KEY,
+ forename VARCHAR(30) NOT NULL,
+ surname VARCHAR(30) NOT NULL,
+ email VARCHAR(30) NOT NULL,
+ gender VARCHAR(5) NOT NULL,
+ in_degree_or_not BOOLEAN ,
+ tutor_name VARCHAR(30)
+ );
+ 
